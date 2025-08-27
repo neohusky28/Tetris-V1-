a1f288afab03a2f3d47c2a3cce950a4ecc90399a
neohusky28 <113532180+neohusky28@users.noreply.github.com>
2025-08-27 18:10:29 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "devsecops-terraform-state-2025" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-west-1"
   }
 }
