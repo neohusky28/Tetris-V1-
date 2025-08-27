5b3b2babe0c392bfdf6585c776cd413e4aa2d8dd
neohusky28 <113532180+neohusky28@users.noreply.github.com>
2025-08-27 18:15:28 +0530

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -1,13 +1,19 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
 }
 
-# Configure the AWS Provider
+variable "aws_region" {
+  type = string
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
+}
+
+# Optional alias, useful for modules
+provider "aws" {
+  alias  = "this"
+  region = var.aws_region
 }
